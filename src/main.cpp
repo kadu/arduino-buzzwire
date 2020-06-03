7a8f95136321ff67d8b72dc673b00d3a40f1fd0e
kadu <kadu@kadu.com.br>
2020-06-03 10:39:28 -0300

    First commit

---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+using namespace std;
+
+#include <Arduino.h>
+#include <BUZZER.h>
+
+#define speaker 11
+#define led 13
+#define sensor 2
+
+BUZZER *Buzzer = new BUZZER(speaker);
+
+void setup() {
+  Serial.begin(115200);
+  Buzzer->setEnable(true);
+
+  pinMode(speaker, OUTPUT);
+  pinMode(led, OUTPUT);
+  pinMode(sensor, INPUT_PULLUP);
+  delay(500);
+  
+
+  for (size_t i = 0; i < 10; i++) {
+    Serial.print(".");
+    digitalWrite(led,!digitalRead(led));
+    delay(200);
+  }
+  Serial.print(".");
+  Serial.println(" Ready!");
+  Buzzer->question(1000);  
+}
+
+void morreu() {
+  digitalWrite(led, HIGH);
+  Buzzer->cancel(2000);
+  Buzzer->question(200); 
+  digitalWrite(led, LOW); 
+}
+
+void loop() {
+  int sensorValue = digitalRead(sensor);
+  Serial.println(sensorValue);
+    
+  if(sensorValue == 0) {
+    morreu();
+  }
+}
